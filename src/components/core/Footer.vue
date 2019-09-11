cc1f570f1c23aad91675667c623868a44de13cd4
Renan <rgmelo94@gmail.com>
2019-09-11 18:15:03 -0300

    first commit

---
+++ src/components/core/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-footer id="core-footer" absolute height="82">
+    <div class="footer-items">
+      <span v-for="link in links" :key="link.name">
+        <a :href="link.Link" class="tertiary--text footer-links">{{ link.name }}</a>
+      </span>
+    </div>
+    <v-spacer />
+    <span class="font-weight-light copyright">
+      &copy;
+      {{ (new Date()).getFullYear() }}
+      <a
+        href="https://www.creative-tim.com/"
+        target="_blank"
+      >Powered by pucom</a>
+      <v-icon color="tertiary" size="17">mdi-heart</v-icon>Economize
+    </span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({})
+};
+</script>
+
+<style>
+#core-footer {
+  margin-top: 20px;
+  z-index: 0;
+}
+</style>
---
+++ src/views/UserProfile.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <material-card color="#320061" title="Editar Loja" text="Atualizar sua loja">
+          <v-form>
+            <v-container class="py-0">
+              <v-row>
+                <v-col cols="12" md="4">
+                  <v-text-field label="Company (disabled)" disabled />
+                </v-col>
+
+                <v-col cols="12" md="4">
+                  <v-text-field class="purple-input" label="User Name" />
+                </v-col>
+
+                <v-col cols="12" md="4">
+                  <v-text-field label="Email Address" class="purple-input" />
+                </v-col>
+
+                <v-col cols="12" md="6">
+                  <v-text-field label="First Name" class="purple-input" />
+                </v-col>
+
+                <v-col cols="12" md="6">
+                  <v-text-field label="Last Name" class="purple-input" />
+                </v-col>
+
+                <v-col cols="12">
+                  <v-text-field label="Adress" class="purple-input" />
+                </v-col>
+
+                <v-col cols="12" md="4">
+                  <v-text-field label="City" class="purple-input" />
+                </v-col>
+
+                <v-col cols="12" md="4">
+                  <v-text-field label="Country" class="purple-input" />
+                </v-col>
+
+                <v-col cols="12" md="4">
+                  <v-text-field class="purple-input" label="Postal Code" type="number" />
+                </v-col>
+
+                <v-col cols="12">
+                  <v-textarea
+                    class="purple-input"
+                    label="About Me"
+                    value="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                  />
+                </v-col>
+
+                <v-col cols="12" class="text-right">
+                  <v-btn color="success">Update Profile</v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </material-card>
+      </v-col>
+      <v-col cols="12" md="4">
+        <material-card class="v-card-profile">
+          <v-avatar slot="offset" class="mx-auto d-block elevation-6" size="130">
+            <img
+              src="https://www.sudoestehoje.com.br/novoportal/wp-content/uploads/2019/06/casas-bahia.jpg"
+            />
+          </v-avatar>
+          <v-card-text class="text-center">
+            <h6 class="overline mb-3">Dona Maria</h6>
+
+            <h4 class="font-weight-light">Dona Maria</h4>
+
+            <p
+              class="font-weight-light"
+            >Don't be scared of the truth because we need to restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the back is...</p>
+          </v-card-text>
+        </material-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>
